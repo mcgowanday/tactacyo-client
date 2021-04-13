6bde5ac1f4050861ef4a9cb278f6994dbebb39bb
mcgowanday <mcgowanfday@gmail.com>
2021-04-13 00:41:42 -0400

    Returned game logic to events. Added 'TIE' logic

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -3,6 +3,9 @@ const getFormFields = require('./../../../lib/get-form-fields')
 const api = require('./api')
 const ui = require('./ui')
 const logic = require('./../game/logic')
+let currentPlayer = 'x'
+let success = false
+let turnNumber = 0
 
 const onSignUp = function (event) {
   event.preventDefault()
@@ -45,7 +48,58 @@ const onNewGame = function (event) {
     .then(ui.onNewGameSuccess)
     .catch(ui.onNewGameFailure)
 }
-let currentPlayer = 'x'
+// const onGameSuccess = function (event) {
+//   if ($('#0').text() === 'x' && $('#1').text() === 'x' && $('#2').text() === 'x') {
+//     $('#message').text('!~!~!~!~!~!~!~!~!~!~!~!~!~!~!  PLAYER X WINS  !~!~!~!~!~!~!~!~!~!~!~!~!~!~!')
+//     success = true
+//   } else if ($('#0').text() === 'o' && $('#1').text() === 'o' && $('#2').text() === 'o') {
+//     $('#message').text('PLAYER O WINS!')
+//     success = true
+//   } else if ($('#3').text() === 'x' && $('#4').text() === 'x' && $('#5').text() === 'x') {
+//     $('#message').text('PLAYER X WINS!')
+//     success = true
+//   } else if ($('#3').text() === 'o' && $('#4').text() === 'o' && $('#5').text() === 'o') {
+//     $('#message').text('PLAYER O WINS!')
+//     success = true
+//   } else if ($('#6').text() === 'x' && $('#7').text() === 'x' && $('#8').text() === 'x') {
+//     $('#message').text('PLAYER X WINS!')
+//     success = true
+//   } else if ($('#6').text() === 'o' && $('#7').text() === 'o' && $('#8').text() === 'o') {
+//     $('#message').text('PLAYER O WINS!')
+//     success = true
+//   } else if ($('#0').text() === 'x' && $('#3').text() === 'x' && $('#6').text() === 'x') {
+//     $('#message').text('PLAYER X WINS!')
+//     success = true
+//   } else if ($('#0').text() === 'o' && $('#3').text() === 'o' && $('#6').text() === 'o') {
+//     $('#message').text('PLAYER O WINS!')
+//     success = true
+//   } else if ($('#1').text() === 'x' && $('#4').text() === 'x' && $('#7').text() === 'x') {
+//     $('#message').text('PLAYER X WINS!')
+//     success = true
+//   } else if ($('#1').text() === 'o' && $('#4').text() === 'o' && $('#7').text() === 'o') {
+//     $('#message').text('PLAYER O WINS!')
+//     success = true
+//   } else if ($('#2').text() === 'x' && $('#5').text() === 'x' && $('#8').text() === 'x') {
+//     $('#message').text('PLAYER X WINS!')
+//     success = true
+//   } else if ($('#2').text() === 'o' && $('#5').text() === 'o' && $('#8').text() === 'o') {
+//     $('#message').text('PLAYER O WINS!')
+//     success = true
+//   } else if ($('#0').text() === 'x' && $('#4').text() === 'x' && $('#8').text() === 'x') {
+//     $('#message').text('PLAYER X WINS!')
+//     success = true
+//   } else if ($('#0').text() === 'o' && $('#4').text() === 'o' && $('#8').text() === 'o') {
+//     $('#message').text('PLAYER O WINS!')
+//     success = true
+//     // number of turns start with 0 ends with 9
+//   } else {
+//     success = false
+//     // this does not work
+//     console.log('not over yet')
+//     // or message of who is next
+//   }
+//   console.log(success)
+// }
 // let gameOver = logic.success
 const onBoxClick = function (event) {
   event.preventDefault()
@@ -54,8 +108,17 @@ const onBoxClick = function (event) {
   // const data = getFormFields(form)
   const box = event.target.id
   const boxText = $(event.target).text()
+  // function checkForTurn() {
+  // let i = 0
+  // for (i = 0; i < 9; i++) {
+  //   // turnNumber = turnNumber + 1
+  //   turnNumber = i
+  //  console.log(turnNumber)
+  // }
+
   // console.log(logic.success)
-  if ($('#message').text() === 'PLAYER O WINS!' || $('#message').text() === 'PLAYER X WINS!') {
+  // $('#message').text() === 'PLAYER O WINS!' || $('#message').text() === 'PLAYER X WINS!'
+  if (success === true) {
     console.log('game over')
     $('#message').text('The game is over!')
   } else if (boxText === 'x' || boxText === 'o') {
@@ -65,15 +128,73 @@ const onBoxClick = function (event) {
   } else {
     $('#' + box).text(currentPlayer)
     $('#message').text('Move logged!')
+    turnNumber = turnNumber + 1
+    console.log(turnNumber)
     if (currentPlayer === 'x') {
       currentPlayer = 'o'
     } else if (currentPlayer === 'o') {
       currentPlayer = 'x'
+    } if ($('#0').text() === 'x' && $('#1').text() === 'x' && $('#2').text() === 'x') {
+      $('#message').text('!~!~!~!~!~!~!~!~!~!~!~!~!~!~!  PLAYER X WINS  !~!~!~!~!~!~!~!~!~!~!~!~!~!~!')
+      success = true
+    } else if ($('#0').text() === 'o' && $('#1').text() === 'o' && $('#2').text() === 'o') {
+      $('#message').text('PLAYER O WINS!')
+      success = true
+    } else if ($('#3').text() === 'x' && $('#4').text() === 'x' && $('#5').text() === 'x') {
+      $('#message').text('PLAYER X WINS!')
+      success = true
+    } else if ($('#3').text() === 'o' && $('#4').text() === 'o' && $('#5').text() === 'o') {
+      $('#message').text('PLAYER O WINS!')
+      success = true
+    } else if ($('#6').text() === 'x' && $('#7').text() === 'x' && $('#8').text() === 'x') {
+      $('#message').text('PLAYER X WINS!')
+      success = true
+    } else if ($('#6').text() === 'o' && $('#7').text() === 'o' && $('#8').text() === 'o') {
+      $('#message').text('PLAYER O WINS!')
+      success = true
+    } else if ($('#0').text() === 'x' && $('#3').text() === 'x' && $('#6').text() === 'x') {
+      $('#message').text('PLAYER X WINS!')
+      success = true
+    } else if ($('#0').text() === 'o' && $('#3').text() === 'o' && $('#6').text() === 'o') {
+      $('#message').text('PLAYER O WINS!')
+      success = true
+    } else if ($('#1').text() === 'x' && $('#4').text() === 'x' && $('#7').text() === 'x') {
+      $('#message').text('PLAYER X WINS!')
+      success = true
+    } else if ($('#1').text() === 'o' && $('#4').text() === 'o' && $('#7').text() === 'o') {
+      $('#message').text('PLAYER O WINS!')
+      success = true
+    } else if ($('#2').text() === 'x' && $('#5').text() === 'x' && $('#8').text() === 'x') {
+      $('#message').text('PLAYER X WINS!')
+      success = true
+    } else if ($('#2').text() === 'o' && $('#5').text() === 'o' && $('#8').text() === 'o') {
+      $('#message').text('PLAYER O WINS!')
+      success = true
+    } else if ($('#0').text() === 'x' && $('#4').text() === 'x' && $('#8').text() === 'x') {
+      $('#message').text('PLAYER X WINS!')
+      success = true
+    } else if ($('#0').text() === 'o' && $('#4').text() === 'o' && $('#8').text() === 'o') {
+      $('#message').text('PLAYER O WINS!')
+      success = true
+    } else if ($('#2').text() === 'x' && $('#4').text() === 'x' && $('#6').text() === 'x') {
+      $('#message').text('PLAYER X WINS!')
+      success = true
+    } else if ($('#2').text() === 'o' && $('#4').text() === 'o' && $('#6').text() === 'o') {
+      $('#message').text('PLAYER O WINS!')
+      success = true
+      // number of turns start with 0 ends with 9
+    } else if (turnNumber === 9) {
+      $('#message').text('TIE GAME!')
+      console.log('not over yet')
+      // or message of who is next
+      console.log(success)
     }
-    // api.boxClick(data/cell)
-    //   .then(ui.onBoxClickSuccess)
-    //   .catch(ui.onBoxClickFailure)
   }
+  // run on game success - check for winner
+
+  // api.boxClick(data/cell)
+  //   .then(ui.onBoxClickSuccess)
+  //   .catch(ui.onBoxClickFailure)
 }
 
 module.exports = {
